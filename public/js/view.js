5bf0422aebd136b522ea02fd1f0aa2d0d2600102
Frank Xin <91763541+xinfrank@users.noreply.github.com>
2021-12-12 00:34:30 -0600

    Initial commit

---
+++ public/js/view.js
@@ -0,0 +1,4 @@
+const toolbar = document.getElementById("view-toolbar-container")
+const width = document.getElementById("view-editor-container").getBoundingClientRect().width
+toolbar.style.width = `${width}px`
+
