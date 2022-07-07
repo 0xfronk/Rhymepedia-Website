bdddbe437281025cd55f02bc56183847d185a770
Frank Xin <91763541+xinfrank@users.noreply.github.com>
2022-07-07 01:13:54 -0400

    Code clean up

--- public/js/view.js
+++
@@ -1,3 +0,0 @@
-/*const toolbar = document.getElementById("view-toolbar-container")
-const width = document.getElementById("view-editor-container").getBoundingClientRect().width
-toolbar.style.width = `${width}px`*/
