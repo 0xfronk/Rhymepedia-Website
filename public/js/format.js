e30bee48e48d3d888fb6e7f14d4cc88e9f6cfcf0
Frank Xin <91763541+xinfrank@users.noreply.github.com>
2022-07-07 01:20:20 -0400

    Code clean up

--- public/js/format.js
+++ public/js/format.js
@@ -1,30 +1,29 @@
-try{
-    const toolbar = document.getElementById("toolbar-container")
-    const width = document.getElementById("editor-container").getBoundingClientRect().width
-    toolbar.style.width = `${width}px`
-    document.getElementById("editor-container").addEventListener("keydown", (function(e) {
-        quill.disable()
-        quill.enable()
-        return false;
-     }))
-     document.getElementById("editor-container").addEventListener("click", (function(e) {
-        quill.disable()
-        quill.enable()
-        return false;
-     }))
+try {
+  const toolbar = document.getElementById("toolbar-container");
+  const width = document
+    .getElementById("editor-container")
+    .getBoundingClientRect().width;
+  toolbar.style.width = `${width}px`;
+  document
+    .getElementById("editor-container")
+    .addEventListener("keydown", function (e) {
+      quill.disable();
+      quill.enable();
+      return false;
+    });
+  document
+    .getElementById("editor-container")
+    .addEventListener("click", function (e) {
+      quill.disable();
+      quill.enable();
+      return false;
+    });
+} catch (error) {}
+try {
+  const search_form = document.getElementById("search-form");
 
-}
-    catch(error){
-        console.log("Quill editor not on this page")
-    }
-try{
-    const search_form = document.getElementById("search-form")
-
-    const search_icon = document.getElementById("search-icon")
-    search_icon.addEventListener("click", (e) => {
-        search_form.submit()
-    })
-}
-    catch(error){
-        console.log("Search box not on this page")
-    }
+  const search_icon = document.getElementById("search-icon");
+  search_icon.addEventListener("click", (e) => {
+    search_form.submit();
+  });
+} catch (error) {}
